c7ad6fc30d7c265a5a9ee1e1c914b707db117bb1
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-07-09 00:12:57 -0300

    chore(dependencies): Add development dependency biome

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,23 @@
-import {fastify} from 'fastify';
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
 import {
-    serializerCompiler,
-    validatorCompiler,
-    type ZodTypeProvider
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
-
-import {fastifyCors} from '@fastify/cors';
 import { env } from './env.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-    origin: 'http://localhost:5173'
-})
+  origin: 'http://localhost:5173',
+});
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.get('/health', () => {
-    return 'OK';
-})
+  return 'OK';
+});
 
-app.listen({port: env.PORT}).then(() => {
-    console.log(`Port: ${process.env.PORT}`);
-    console.log('HTTP server running!!!');
-})
+app.listen({ port: env.PORT });
