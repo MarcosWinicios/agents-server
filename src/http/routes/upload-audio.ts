4ed339e3ead131be3263b0184dba550654739055
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-07-16 21:32:49 -0300

    feat(database): Add audio-chunks table migration

---
+++ src/http/routes/upload-audio.ts
@@ -0,0 +1,29 @@
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
+import { z } from 'zod/v4';
+
+export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
+  app.post(
+    '/rooms/:roomId/audio',
+    {
+      schema: {
+        params: z.object({
+          roomId: z.string(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { roomId } = request.params;
+      const audio = await request.file();
+
+      if (!audio) {
+        throw new Error('Audio is required');
+      }
+
+      //1. Transcrever o áudio
+
+      //2. Gerar o vetor semântico / embeddings
+
+      //3. Armezenar os vetores no banco de dados
+    }
+  );
+};
